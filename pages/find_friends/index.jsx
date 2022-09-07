5a8889ce21806d9e537a2056c1d20a488eba7100
FaisalMuhammed <faisal707974@gmail.com>
2022-09-07 10:31:19 +0530

    navbar added to all pages

--- pages/find_friends/index.jsx
+++ pages/find_friends/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import NavBar from '../../components/NavBar/NavBar'
 
 export default function index() {
     return (
         <div>
-            <h1>find friends</h1>
+            <NavBar/>
         </div>
     )
 }
--- pages/wallet/index.jsx
+++ pages/wallet/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import NavBar from '../../components/NavBar/NavBar'
 
 export default function index() {
     return (
         <div>
-            <h1>wallet</h1>
+            <NavBar/>
         </div>
     )
 }
