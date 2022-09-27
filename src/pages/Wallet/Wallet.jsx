32228945eac09a20c425e3dba3508721a6797e66
FaisalMuhammed <faisal707974@gmail.com>
2022-09-27 07:33:20 +0530

    wallet ui  completed

--- src/pages/Wallet/Wallet.jsx
+++ src/pages/Wallet/Wallet.jsx
@@ -4,21 +4,33 @@ import './Wallet.scss'
 import NavBar from '../../components/NavBar/NavBar'
 import Header from '../../components/Header/Header'
 
+import image_qrcode from '../../asset/images/Wallet/qrcode.png'
+
 export default function Wallet() {
     return (
         <div className='Wallet grid'>
             <Header />
             <NavBar currPage={'Wallet'} />
             <div className='wallet_content'>
-                <p>EMC Balance: $</p>
-                <p>USD Balance:</p>
+                <p data-content="heading">EMC Balance: $</p>
+                <p data-content="heading">USD Balance:</p>
                 <div className="transactions">
-                    <faisal>Transactions</faisal>
+                    <h3>Transactions</h3>
+                    <p>No transactions available</p>
                 </div>
                 <div className='qrcode'>
-                    <div className="qr"></div>
-                    <div className="wallet_address"></div>
-                    <div className="send_to"></div>
+                    <div className="qr">
+                        <img src={image_qrcode} alt="" />
+                    </div>
+                    <div className="wallet_address">
+                        <h6>Wallet Address</h6>
+                        <p>0x4c20c06ff5BFDfasdfasdfasdfasfasdf</p>
+                    </div>
+                    <div className="send_to">
+                        <p>Send To A Public Address</p>
+                        <input type="text" placeholder='Public / Private key' />
+                        <button>Send</button>
+                    </div>
                 </div>
             </div>
         </div>
