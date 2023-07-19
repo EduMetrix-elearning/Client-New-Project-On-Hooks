14d426849e5d4f0987591ae06034d57c72a5ddf8
Mridu-devs <mridu.devs@gmail.com>
2023-07-19 16:07:03 +0530

    added modal for EmployeProfile and temp profilepic

--- src/frontpage/MainDashBoard/Intern/InternNavbar.jsx
+++ src/frontpage/MainDashBoard/Intern/InternNavbar.jsx
@@ -1,24 +1,100 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./InternNavbar.css";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import HomeIcon from "@mui/icons-material/Home";
+import EmployeeProfile from "../../../components/employee_profile/EmployeeProfile";
 // import { DashBoardInputs } from "../../../WebsiteDashboard/Inputs";
 // import { DashBoardDetails } from "../../../WebsiteDashboard/Details";
 // import { DashBoardStatus } from "../../../WebsiteDashboard/Status";
 
+import "react-responsive-modal/styles.css";
+import { Modal } from "react-responsive-modal";
+
 const InternNavbar = () => {
   const navigate = useNavigate();
+  const [empId, setEmpId] = useState("");
+  const [username, setUsername] = useState("");
+  const [profilePic, setProfilePic] = useState("");
+
+  const [open, setOpen] = useState(false);
+
+  const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
+
+  useEffect(() => {
+    setEmpId(localStorage.getItem("employeeid"));
+    setUsername(localStorage.getItem("employeeName"));
+    setProfilePic(localStorage.getItem("employeeProfilePic"));
+  }, []);
+
   return (
     <>
       <div className="side_bar">
-        <div className="logo">
+        {/* <div className="logo">
           <img
             className="logo-img"
             src="https://edumetrix.io/static/media/coin.c062b2cf16bee63f2da9.png"
             alt=""
           />
+        </div> */}
+        <div className="logo" onClick={onOpenModal}>
+          {/* <Link
+            to="employeeprofile"
+            style={{
+              textDecoration: "none",
+              display: "flex",
+              alignItems: "end",
+              gap: "10px",
+            }}
+          > */}
+          <div
+            style={{
+              textDecoration: "none",
+              display: "flex",
+              alignItems: "end",
+              gap: "10px",
+              cursor: "pointer",
+            }}
+          >
+            <img
+              src="https://cdn.britannica.com/92/215392-050-96A4BC1D/Australian-actor-Chris-Hemsworth-2019.jpg"
+              style={{
+                width: "80px",
+                height: "80px",
+                borderRadius: "50%",
+                objectFit: "cover",
+                marginLeft: "20px",
+              }}
+            />
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              {empId && (
+                <span
+                  style={{
+                    color: "black",
+                    fontSize: "15px",
+                    fontWeight: "800",
+                  }}
+                >
+                  {empId}
+                </span>
+              )}
+              {username && (
+                <span
+                  style={{
+                    color: "black",
+                    fontSize: "25px",
+                    fontWeight: "800",
+                  }}
+                >
+                  {username}
+                </span>
+              )}
+            </div>
+            {/* </Link> */}
+          </div>
         </div>
+        {/* <EmployeeProfile /> */}
 
         <ul>
           {/* <li>
@@ -50,6 +126,11 @@ const InternNavbar = () => {
             logout
           </button>
         </div>
+      </div>
+      <div>
+        <Modal open={open} onClose={onCloseModal} center>
+          <EmployeeProfile />
+        </Modal>
       </div>
     </>
   );
